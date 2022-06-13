38d62fd70a8587a9ffafdb06ce41f8d36e0183d1
kbodanko <62223496+kbodanko@users.noreply.github.com>
2022-06-14 00:02:34 +0200

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -151,7 +151,7 @@ winePage.addEventListener('click', scrollToMore);
 //     mailIcon.classList.remove('animate__headShake');
 // });
 let designSlideContent = ['xd/mob/good.png', 'xd/mounts.png', 'xd/phones4.png', 'xd/Group12.png', 'xd/beuty_trnsp.png'];
-let webSlideContent = ['xd/mob/Lovewines1.png', 'xd/mob/doc.png', 'xd/mob/BigBangGame.png', 'xd/mob/Konfigurator.png'];
+let webSlideContent = ['xd/mob/Lovewines1.png', 'xd/mob/doc.png', 'xd/mob/BigBangGame.png', 'xd/mob/Konfigurator.png',];
 let webSlideUrl = ['https://kbodanko.github.io/wine-page/', 'https://kbodanko.github.io/doc/', 'https://kbodanko.github.io/BigBangGame/', 'https://kbodanko.github.io/Layan_konfigurator/'];
 let slidesDesign = document.querySelectorAll('.swiper_slide_design');
 function fillDesignSlider() {
