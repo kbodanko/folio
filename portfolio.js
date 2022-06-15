433c0ff515b4847e77dac28d079b93767f96e78b
kbodanko <62223496+kbodanko@users.noreply.github.com>
2022-06-15 19:25:30 +0200

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -150,7 +150,7 @@ winePage.addEventListener('click', scrollToMore);
 // mailIcon.addEventListener('mouseleave', function () {
 //     mailIcon.classList.remove('animate__headShake');
 // });
-let designSlideContent = ["xd/mob/good.png", "xd/mounts.png", "xd/phones4.png", "xd/ala.png", "xd/Group12.png", "xd/Desktop.png"];
+let designSlideContent = ["xd/mob/good.png", "xd/mounts.png", "xd/phones4.png", "xd/ala.png", "xd/Group12.png", "xd/Desktop2.png"];
 
 // let designSlideContent = ['xd/mob/good.png', 'xd/mounts.png', 'xd/phones4.png', "http://all4desktop.com/data_images/original/4189613-digital-abstract-eye-2.jpg"];
 let webSlideContent = ['xd/mob/wine.png', 'xd/mob/doc.png', 'xd/mob/BigBangGame.png', 'xd/mob/Konfigurator.png',];
